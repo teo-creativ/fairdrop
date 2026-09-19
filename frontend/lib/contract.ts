6f6ebfd3cf271bd96aa5efb67bcba384e3b7add4
teo-creativ <teoszucs71@gmail.com>
2026-09-19 12:17:26 +0200

    Update contract address for Monad Testnet

--- frontend/lib/contract.ts
+++ frontend/lib/contract.ts
@@ -171,4 +171,4 @@ export const fairDropAbi = [
 ] as const;
 
 // TODO : remplace par ta vraie adresse déployée
-export const fairDropAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" as const;
+export const fairDropAddress = "0x908858BcED4E90e7Cf11Ea388d5E4153769508e0" as const;
